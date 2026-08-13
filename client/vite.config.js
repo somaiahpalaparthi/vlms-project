1af778b0bbbea6f620cd21216ea0d4e08832929b
somaiahpalaparthi <140062874+somaiahpalaparthi@users.noreply.github.com>
2026-08-13 09:29:43 +0530

    ds

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://vlms-project.onrender.com',
         changeOrigin: true,
       },
     },
